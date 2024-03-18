3283eaf3c1dec64cc31b7e4c5d06d431d337235e
daniel lasota <slasher.root@gmail.com>
2024-03-18 21:33:13 +0100

    error sending add to FlaskConsoleLogger

--- flask_stream_manager/stream_to_logger.py
+++ flask_stream_manager/stream_to_logger.py
@@ -7,13 +7,24 @@ class FlaskConsoleLogger(Observer):
     def __init__(self):
         super().__init__()
         self.original_stdout = sys.stdout
+        self.original_stderr = sys.stderr
+        sys.stdout = self
+        sys.stderr = self
 
     def write(self, message):
+        if sys.exc_info() != (None, None, None):
+            message_type = 'error'
+        else:
+            message_type = 'output'
+
         self.notify_observers({'matrixModeGlobalConsole': message})
+
         self.original_stdout.write(message)
 
     def flush(self):
         self.original_stdout.flush()
+        self.original_stderr.flush()
 
     def close(self):
         sys.stdout = self.original_stdout
+        sys.stderr = self.original_stderr
